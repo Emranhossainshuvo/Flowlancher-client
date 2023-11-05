2b0a1690ad0878237e914fff9feaa6ab8117c10e
Shuvo <shuvoorba28@gmail.com>
2023-11-05 11:18:58 -0800

    register page added

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   </>;
 
   return (
-    <div className="navbar bg-transparent background-img max-w-7xl mx-auto bg-base-100">
+    <div className="navbar bg-transparent background-img text-white max-w-7xl mx-auto bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
