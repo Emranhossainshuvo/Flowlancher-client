ba84d132fa26c576d5bd86b1389080e9d467c1b1
Shuvo <shuvoorba28@gmail.com>
2023-11-08 14:50:38 -0800

    my posted jobs showed in the ui

--- src/pages/MyPostedJob/MyPostedJob.jsx
+++ src/pages/MyPostedJob/MyPostedJob.jsx
@@ -4,6 +4,7 @@ import Footer from '../shared/Footer/Footer';
 import { Helmet } from 'react-helmet';
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
+import MyPostedJobRow from './MyPostedJobRow';
 
 const MyPostedJob = () => {
 
@@ -25,67 +26,34 @@ const MyPostedJob = () => {
                 <Helmet><title>FlowLancher | My posted jobs</title></Helmet>
                 {/* user specific job will code here */}
 
-                <div className="relative pt-20 overflow-x-auto">
+                <div className="relative pt-28 pb-28 overflow-x-auto">
                     <table className="w-full text-white font-semibold text-sm text-left ">
                         <thead className="text-xs  uppercase   text-white font-semibold">
                             <tr>
                                 <th scope="col" className="px-6 text-lg py-3">
-                                    Product name
+                                    Job title
                                 </th>
                                 <th scope="col" className="px-6 text-lg py-3">
-                                    Color
+                                    Min. price
                                 </th>
                                 <th scope="col" className="px-6 text-lg py-3">
-                                    Category
+                                    Max. price
                                 </th>
                                 <th scope="col" className="px-6 text-lg py-3">
-                                    Price
+                                    Deadline
+                                </th>
+                                <th scope="col" className="px-6 text-lg py-3">
+                                    Action
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className=" border-b  dark:border-gray-700">
-                                <th scope="row" className="px-6 text-lg py-4  text-white font-semibold whitespace-nowrap">
-                                    Apple MacBook Pro 17"
-                                </th>
-                                <td className="px-6 text-lg py-4">
-                                    Silver
-                                </td>
-                                <td className="px-6 text-lg py-4">
-                                    Laptop
-                                </td>
-                                <td className="px-6 text-lg py-4">
-                                    $2999
-                                </td>
-                            </tr>
-                            <tr className=" border-b  dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4  text-white font-semibold whitespace-nowrap text-lg dark:text-white ">
-                                    Microsoft Surface Pro
-                                </th>
-                                <td className="px-6 text-lg py-4">
-                                    White
-                                </td>
-                                <td className="px-6 text-lg py-4">
-                                    Laptop PC
-                                </td>
-                                <td className="px-6 text-lg py-4">
-                                    $1999
-                                </td>
-                            </tr>
-                            <tr className=" ">
-                                <th scope="row" className="px-6 text-lg py-4 text-white font-semibold whitespace-nowrap dark:text-white ">
-                                    Magic Mouse 2
-                                </th>
-                                <td className="px-6 text-lg py-4">
-                                    Black
-                                </td>
-                                <td className="px-6 text-lg py-4">
-                                    Accessories
-                                </td>
-                                <td className="px-6 text-lg py-4">
-                                    $99
-                                </td>
-                            </tr>
+                            {
+                                jobs.map(job => <MyPostedJobRow key={job._id}
+
+                                    job={job}
+                                ></MyPostedJobRow>)
+                            }
                         </tbody>
                     </table>
                 </div>
---
+++ src/pages/MyPostedJob/MyPostedJobRow.jsx
@@ -0,0 +1,34 @@
+
+
+const MyPostedJobRow = ({ job }) => {
+
+    const { _id, title, description, maximum, minimum, deadline, select } = job || {}
+
+    return (
+        <tr className=" border-b  dark:border-gray-700">
+            <th scope="row" className="px-6 text-lg flex justify-between items-center py-4  text-white font-semibold whitespace-nowrap">
+                {title}
+                <button className="btn bg-white btn-square btn-outline">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </th>
+
+            <td className="px-6 text-lg py-4">
+                ${minimum}
+            </td>
+            <td className="px-6 text-lg py-4">
+                ${maximum}
+            </td>
+            <td className="px-6 text-lg py-4">
+                {deadline}
+            </td>
+            <td className="px-6 text-lg py-4">
+                <button className="btn hover:btn-neutral">
+                    Update
+                </button>
+            </td>
+        </tr>
+    );
+};
+
+export default MyPostedJobRow;
